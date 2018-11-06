39fa0fd9362e58e6bc034e63c364920ad1323ad0
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-11-05 22:14:16 -0700

    Favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import '../index.css'
+import favicon from '../favicon.png'
+import Helmet from 'react-helmet'
 
 const IndexPage = () => (
   <div className='container'>
+    <Helmet
+      title="Shawn McGinnis Law - St. George, Utah Personal Injury Lawyer"
+      meta={[
+          { name: 'description', content: 'Shawn McGinnis Law - St. George, Utah Personal Injury Lawyer' },
+          { name: 'keywords', content: 'Shawn McGinnis Law - St. George, Utah Personal Injury Lawyer' },
+      ]}
+      link={[
+          { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+      ]}
+    />
     <img
       style={{width: 700}}
       src={require('../images/logo-bg.jpg')}
